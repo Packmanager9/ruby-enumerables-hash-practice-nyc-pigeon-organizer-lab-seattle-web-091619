fc1fc123a38a49c6916bb38039a4adb263e22fb6
Cole Ditzler <colejditzler@gmail.com>
2019-09-16 04:16:09 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+
+
+
+
+  exitpidgeondata = {}
+  arrayofnames = []
+
+i = 0
+while arrayofnames.length > i
+
+  arrayofnames = data.values.values_at(i)
+  exitpidgeondata[arrayofnames[i]] = "names"
+  i = i+1
+end
+
+
+
+p arrayofnames
+  puts  exitpidgeondata
+exitpidgeondata
 end
